267e2250fc6a0bfaea427a3b481c5ddc57d28d86
Johannes Camin <heregoesthesun@gmx.de>
2017-09-01 19:59:49 +0200

    App Komponente wird sehr wahrscheinlich nicht ohne state bleiben, von daher in eine Klasse umgewandelt

--- bin/components/App.jsx
+++ bin/components/App.jsx
@@ -1,25 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => (
-  <div id="container">
-    <header>
-      <img src="bin/img/title.png" alt="Troubadour" />
-    </header>
+export default class App extends Component {
+  render() {
+    return (
+      <div id="container">
+        <header>
+          <img src="bin/img/title.png" alt="Troubadour" />
+        </header>
 
-    <div id="player">
-      <div
-        className="stimmung"
-        id="neu"
-        onClick={() => {
-          console.log("clicked add button");
-        }}
-      >
-        <i className="fa fa-plus" aria-hidden="true" />
+        <div id="player">
+          <div
+            className="stimmung"
+            id="neu"
+            onClick={() => {
+              console.log("clicked add button");
+            }}
+          >
+            <i className="fa fa-plus" aria-hidden="true" />
+          </div>
+        </div>
+
+        <div id="eigenschaften" />
       </div>
-    </div>
-
-    <div id="eigenschaften" />
-  </div>
-);
-
-export default App;
+    );
+  }
+}
